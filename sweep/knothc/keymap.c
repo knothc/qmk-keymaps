1bb5ec290e109f0d14833864d260da42eb9432d0
Efraín Mejía <efrain.mejia@justeattakeaway.com>
2023-06-04 23:59:03 +0800

    [sweep] home-row numerics on high

--- sweep/knothc/keymap.c
+++ sweep/knothc/keymap.c
@@ -56,13 +56,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 		KCA_NAV, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
 		SHFT_Z,  KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  SHFT_SLSH,
-															 KC_LGUI, ML_LO,           HI_SPC,  SYM
+								   KC_LGUI, ML_LO,           HI_SPC,  SYM
     ),
 
+    // [_HIGH] = LAYOUT_split_3x5_2(  // "RAISE"
+	// 	KC_ESC, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,       KC_VOLU, KC_7, KC_8, KC_9, KC_DEL,
+	// 	KC_TAB, ALFRED,  OSM_ACG, OSM_AC,  XXXXXXX,       KC_VOLD, KC_4, KC_5, KC_6, KC_0,
+	// 	QTERM,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_MUTE, KC_1, KC_2, KC_3, TMUXP,
+    //                               _______, _______,       _______,  _______
+    // ),
     [_HIGH] = LAYOUT_split_3x5_2(  // "RAISE"
-		KC_ESC, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,       KC_VOLU, KC_7, KC_8, KC_9, KC_DEL,
-		KC_TAB, ALFRED,  OSM_ACG, OSM_AC,  XXXXXXX,       KC_VOLD, KC_4, KC_5, KC_6, KC_0,
-		QTERM,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_MUTE, KC_1, KC_2, KC_3, TMUXP,
+		KC_ESC, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,       XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE, KC_DEL,
+		KC_1,   KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+		QTERM,  OSM_ACG, XXXXXXX, ALFRED,  XXXXXXX,       XXXXXXX, OSM_AC, XXXXXXX, XXXXXXX, TMUXP,
                                   _______, _______,       _______,  _______
     ),
 
