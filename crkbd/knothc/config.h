edf0894b1873547ab12f224e853a4ca5700a043a
Efraín Mejía <efrain.mejia@justeattakeaway.com>
2024-05-16 12:24:06 +0800

    [crkbd] remove MOD_TAP_INTERRUPT

--- crkbd/knothc/config.h
+++ crkbd/knothc/config.h
@@ -29,9 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
